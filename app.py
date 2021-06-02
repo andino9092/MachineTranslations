77f1259c7fa4ccef812fe4c3f938dab06e62eaea
Andy <andyli9092@gmail.com>
2021-06-02 18:06:12 -0400

    Added enhance image function and need to look  into OCR engine

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from langdetect import detect
-from PIL import Image
+from PIL import Image, ImageEnhance
 from googletrans import Translator, constants
 from pprint import pprint
 import pytesseract
+
+def imageEnhance(image):
+    img = Image.open(image)
+    img = img.convert('L')
+    # enhancer = ImageEnhance.Contrast(img)
+    # newImg = enhancer.enhance(2)
+    return img
 
 def locateText():
 
@@ -17,12 +24,12 @@ def translate():
 def getStringFromImage():
     return
 
-pytesseract.pytesseract.tesseract_cmd = r'/opt/local/bin/tesseract'
-img = Image.open("TestingPics/Lookism2_kor.png")
-img = img.convert('L')
+pytesseract.pytesseract.tesseract_cmd = r'Z:\Tesseract\tesseract'
+#pytesseract.pytesseract.tesseract_cmd = r'/opt/local/bin/tesseract'
+imageLocation = "TestingPics/Lookism_kor.png"
 
-find = pytesseract.image_to_data(img, lang="kor")
-print(find[0])
-lookism = pytesseract.image_to_string(img, lang="kor")
-data = pytesseract.image_to_string(r'TestingPics/KoreanRegon.PNG', lang = "kor")
-print(lookism)
+img = imageEnhance(imageLocation)
+text = pytesseract.image_to_string(img, lang = "kor+eng", config='psm 12')
+print(text)
+img1 = img.save("TestingPics/Lookism2_kor_edited.png")
+#find = pytesseract.image_to_data(img, lang="kor").split("\n")
