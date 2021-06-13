604f4bde0ff853fd054bd910c60617417570ce4f
Andy <andyli9092@gmail.com>
2021-06-12 23:28:10 -0400

    Added whiten function

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from google_trans_new import google_translator
 import easyocr
 import string
 
+from torch.utils import data
+
+editLocation = "TestingPics/Edit.png"
+lang = "ch_tra"
+
 class Translation:
     def __init__(self, boxes, translation):
         self.boxes = boxes
@@ -14,34 +19,38 @@ class Translation:
         return self.translation
     def getBoxes(self):
         return self.boxes
-    
-editLocation = "TestingPics/Edit.png"
-lang = "ch_tra"
+
+class Box:
+    def __init__(self, a, b, c, d):
+        self.topLeft = a
+        self.topRight = b
+        self.bottomLeft = c
+        self.bottomRight = d
+    def getTopLeft(self):
+        return self.topLeft
+    def getTopRight(self):
+        return self.topRight
+    def getBottomLeft(self):
+        return self.bottomLeft
+    def getBottomRight(self):
+        return self.bottomRight
 
 def load():
     global reader
     reader = easyocr.Reader(['ch_tra'], gpu = False)
 
 def loadImage(imageLocation):
+    global image
     image = Image.open(imageLocation)
     imgSave = imageEnhance(image).save(editLocation)
     global rawImageInfo
     rawImageInfo = reader.readtext(editLocation, paragraph=True)
-
 
 def imageEnhance(image):
     img = image.convert('L')
     enhancer = ImageEnhance.Contrast(img)
     newImg = enhancer.enhance(5)
     return newImg
-
-def locateText():
-    #TODO
-    return
-
-def locateTextBox():
-    #TODO
-    return
 
 # make sure its a phrase and not just symbols
 def langCheck(phrase):
@@ -86,7 +95,20 @@ def translateText():
         text.append(data[1])
     translated = GoogleTranslator(source = "auto", target = "en").translate_batch(text)
     return translated
-
+    
+def whiten():
+    for key in dataDict:
+        boxes = dataDict[key].getBoxes()
+        for i in range(boxes[0][0], boxes[1][0]):
+            for m in range(boxes[0][1], boxes[2][1]):
+                image.putpixel( (i,m), (255, 255, 255))
+    imgSave = image.save(editLocation)
+# [[383, 63], [457, 63], [457, 99], [383, 99]]
+# [[614, 52], [807, 52], [807, 119], [614, 119]]
+# [[64, 546], [243, 546], [243, 610], [64, 610]]
+# [[645, 677], [773, 677], [773, 709], [645, 709]]
+# [[698, 732], [802, 732], [802, 794], [698, 794]]
+# [[99, 877], [239, 877], [239, 917], [99, 917]]
 def translateImage():
     #TODO
     return
@@ -97,7 +119,10 @@ def main():
     removeNoise()
     translated = translateText()
     createDictionary(translated)
-
+    whiten()
 main()
 
-#maybe use machine learning to train a bot to understand informal korean
+
+# Dictionary for boxes: dataDict
+# Array for raw text: langText
+# Idea for another project: maybe use machine learning to train a bot to understand informal korean
